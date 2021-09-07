60de693ba6ca9faa53322fa8b957bd86bd21a932
Rinat Ashurko <rinat.ashurko@mail.ru>
2021-09-07 20:12:02 +0300

    event mobile

--- js/script.js
+++ js/script.js
@@ -1,74 +1,37 @@
 'use strict';
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres:[],
-    privat: true,
-    start: function () {
-        personalMovieDB.count = prompt('Сколько фильмов вы уже посомтрели?', '');
+// touchstart
+// touchend
+// touchmove
+// touchenter
+// touchleave
+// touchcancel
 
-    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-        personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
-   }
- },    
-    remember: function () {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
 
-        if (a != null && b != null && a != '' && b != '' ) {
-            personalMovieDB.movies[a] = b;
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-},
-    checkMyAnswers: function () {
-    if (personalMovieDB.count < 10) {
-        console.log('Просомотренно мало фильмов');
-    }   else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-            console.log('Вы классический зритель');
-    }  else if (personalMovieDB.count > 30) {
-        console.log('Вы киноман');
-    }
-},
-    showMyDB: function () {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    }
-},
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
 
-    writeYourgenres: function () {
-    for (let i =1; i <2; i++) {
-//     let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-//     if (genre === '' || genre === null) {
-//         i--;
-//     } else {
-//         personalMovieDB.genres[i-1] = genre;
-//     }
-        let genre = prompt(`Введите свои любимые жанры черзе запятую`).toLowerCase();
-        
-        if (genre === '' || genre === null) {
-        i--;
-        } else {
-        personalMovieDB.genres = genre.split(', ');
-        personalMovieDB.genres.sort();
-        }
-    }
-   personalMovieDB.genres.forEach((item, i) => {
-       console.log(`Любимый жанр ${i +1} - это ${item}`);
-   });
-},
-    toggleVisibleMyDB: function () {
-        if (personalMovieDB.privat){
-            personalMovieDB.privat = false;
-        }else {
-            personalMovieDB.privat = true;
-        }
-    }
-};
-console.log(personalMovieDB);
+        console.log('start');
+        console.log(e.touches);
+        console.log(e.targetTouches);
+    });
+    box.addEventListener('touchend', (e) => {
+        e.preventDefault();
 
+        console.log('end');
+    });
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log(e.targetTouches[0].pageX);
+    });
+   
+});
+
+//touches 
+//targetTouches
+//changedTouches
+
+// swipe install Hammer.js programm
