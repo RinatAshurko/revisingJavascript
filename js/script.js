23b16775e603cab2e6ee7ad5c63073aa85e07170
Rinat Ashurko <rinat.ashurko@mail.ru>
2021-09-13 21:44:02 +0300

    array iteration methods

--- js/script.js
+++ js/script.js
@@ -1,49 +1,61 @@
 'use strict';
 
-// promise
-console.log('Запрос данных');
+//filter
 
-const req = new Promise((resolve, reject) => {
-    
-    setTimeout(() => {
+const names = ['Ann', 'Ivan', 'ALexander', 'Voldemart'];
 
-        console.log('Подготовка данных...');
-        const product = {
-            name: 'Alex',
-            phone: 1
-        };
-        resolve(product);
-    }, 2000);
-});
-req.then((data) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.number = 375;
-            console.log('Данные полученны');
-            console.log(data);
-      
-            resolve(data);
-        }, 2000);
-    });
-}).then(data => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.number = 455;
-            console.log(data);
-            console.log('Данные измененны');
-            resolve(data);
-        }, 2000);
-    });
-}).finally(() => {
-    setTimeout(() => {
-        console.log('Операция прошла успешно');
-    }, 2000);
+const shortName = names.filter((name) => {
+    return name.length <5;
 });
 
+console.log(shortName);
+
+//map 
+
+const answers = ['IvAN', 'Ann', 'Heloo'];
+
+const results = answers.map(item => item.toLowerCase());
+console.log(results);
+
+// every/some
+
+const some = [4, 'Ivan', 'ahah'];
+
+console.log(some.some(item => typeof(item) === 'number')); // Если хотя бы одно значения является числом вернет true
+
+console.log(some.every(item => typeof(item) === 'number')); //Если каждый элемент типа:число, то вернет true 
 
 
+//reduce 
+
+const arr = [4, 2, 5, 6, 7];
+                        // 0/3     4
+                        // 4     2
+                        // 6     5
+                        // 11    6
+                        // 17    7
+                        
+const res = arr.reduce((sum, current) => sum + current, 3);
+console.log(res);
 
 
+const arr1 = ['plum', 'apple', 'banana'];
+
+const ress = arr1.reduce((sum, current) => (`${sum}, ${current}`));
+console.log(ress);
+
+
+//example with text content
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+console.log(newArr);
 
 
 
