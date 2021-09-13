6137773c8cb16df78141a2f204ec8d1106f93379
Rinat Ashurko <rinat.ashurko@mail.ru>
2021-09-13 14:27:57 +0300

    promise

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
 'use strict';
 
-const persone = {
-    name: 'Alex',
-    age: 26
-};
+// promise
+console.log('Запрос данных');
 
-console.log(JSON.stringify(persone));
-console.log(JSON.parse(JSON.stringify(persone)));
+const req = new Promise((resolve, reject) => {
+    
+    setTimeout(() => {
+
+        console.log('Подготовка данных...');
+        const product = {
+            name: 'Alex',
+            phone: 1
+        };
+        resolve(product);
+    }, 2000);
+});
+req.then((data) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.number = 375;
+            console.log('Данные полученны');
+            console.log(data);
+      
+            resolve(data);
+        }, 2000);
+    });
+}).then(data => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.number = 455;
+            console.log(data);
+            console.log('Данные измененны');
+            resolve(data);
+        }, 2000);
+    });
+}).finally(() => {
+    setTimeout(() => {
+        console.log('Операция прошла успешно');
+    }, 2000);
+});
 
 
 
